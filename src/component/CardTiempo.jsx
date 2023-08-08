c7f2bfc53aab875e8f3d6e658591c233cbc7e587
Nico Almiron <almiron.nicolas16@gmail.com>
2023-08-08 12:30:53 -0300

    se corrigieron algunos detalles de la maquetacion

--- src/component/CardTiempo.jsx
+++ src/component/CardTiempo.jsx
@@ -4,30 +4,41 @@ import { Card, Col, Image, Row } from "react-bootstrap";
 const CardTiempo = () => {
   return (
     <Card className="bg-dark text-white m-4" border="primary">
-      <Card.Header>TIEMPO ACTUAL</Card.Header>
+      <Card.Header className="text-center text-warning">
+        TIEMPO ACTUAL
+      </Card.Header>
       <Card.Body>
-        <Card.Title>Argentina, Tucumán</Card.Title>
+        <Card.Title className="text-center pb-3">Argentina, Tucumán</Card.Title>
         <div>
-          <Row>
+          <Row className="flex-column flex-md-row justify-content-center align-items-center">
             <Col xs={6} className="d-flex">
-              <Image src="src/assets/iconList/02d.png" rounded></Image>
-              <div>
-                <p className="display-4">25ºc</p>
-                <p className="fw-light">25º/13º</p>
+              <Image
+                src="src/assets/iconList/02d.png"
+                rounded
+                className="d-none d-sm-flex"
+              ></Image>
+              <div className="text-center">
+                <p className="display-4">20ºc</p>
+                <p className="fw-bold">
+                  25º<i className="fa-solid fa-plus fa-2xs p-1"></i>/13º
+                  <i className="fa-solid fa-minus fa-2xs p-1"></i>
+                </p>
               </div>
             </Col>
-            <Col xs={6} className="">
+            <Col xs={6} className="d-flex flex-row flex-md-column text-center">
               <p className="fw-light">
-                Sensacion Termica
-                <i className="fa-solid fa-temperature-low p-1"></i>
+                <span className="d-none d-md-inline">Sensacion Termica</span>
+                <i className="fa-solid fa-temperature-low p-2"></i>
                 <span className="fw-bold p-2">25º</span>
               </p>
               <p className="fw-light">
-                Viento <i className="fa-solid fa-wind p-1"></i>
+                <span className="d-none d-md-inline">Viento</span>
+                <i className="fa-solid fa-wind p-2"></i>
                 <span className="fw-bold p-2">25m/s</span>
               </p>
               <p className="fw-light">
-                Huemadad <i className="fa-solid fa-water p-1"></i>
+                <span className="d-none d-md-inline">Huemadad</span>
+                <i className="fa-solid fa-water p-2"></i>
                 <span className="fw-bold p-2">25%</span>
               </p>
             </Col>
